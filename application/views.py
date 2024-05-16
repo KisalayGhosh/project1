519fc8aa874fdff73fbd9cbb1199595169a2498e
KisalayGhosh <kisalay.ghsh20027@gmail.com>
2024-05-16 14:33:35 +0530

    loading the frontend, db and other files

---
+++ application/views.py
@@ -0,0 +1,5 @@
+from flask import current_app as app, render_template
+
+@app.get('/')
+def home():
+    return render_template("index.html")
---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from application.models import db
+from config import DevelopmentConfig
+
+def create_app():
+    app = Flask(__name__)
+    db.init_app(app)
+    app.config.from_object(DevelopmentConfig)
+    with app.app_context():
+        import application.views
+        
+    return app
+
+app=create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
