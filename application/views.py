b701403b8b1b66a3a10fb7a9f3e4827d56a17561
Kisalay Ghosh <87713832+KisalayGhosh@users.noreply.github.com>
2024-07-03 18:21:32 +0530

    RBAC

--- application/views.py
+++ application/views.py
@@ -1,5 +1,13 @@
 from flask import current_app as app, render_template
+from flask_security import auth_required, roles_required 
 
 @app.get('/')
 def home():
     return render_template("index.html")
+
+
+@app.get('/admininfo')
+@auth_required("token")
+@roles_required("admin")
+def admininfo():
+    return "Nice to have yoy back on th epage Admin!!"
